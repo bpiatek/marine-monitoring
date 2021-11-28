15039ce989fd2870d8492823003f49717cf0c1b7
B. Piatek <piatekbart@gmail.com>
2021-11-28 16:35:40 +0100

    initial commit

---
+++ src/main/java/pl/bpiatek/marinemonitoring/MarineMonitoringApplication.java
@@ -0,0 +1,12 @@
+package pl.bpiatek.marinemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarineMonitoringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MarineMonitoringApplication.class, args);
+  }
+}
---
+++ src/test/java/pl/bpiatek/marinemonitoring/MarineMonitoringApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.bpiatek.marinemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarineMonitoringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
